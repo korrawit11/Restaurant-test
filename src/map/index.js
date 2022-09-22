9acdb4db981f8819f5f1674a016000608f9f7b7e
korrawit11 <korrawit081@gmail.com>
2022-09-22 22:43:25 +0700

    add api key

---
+++ src/map/index.js
@@ -0,0 +1,12 @@
+<script async
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBwPOGB3OQTWUd2SfoE7ojPNbeXl8MAaqU&libraries=places&callback=initMap">
+</script>
+
+const key = 'AIzaSyBwPOGB3OQTWUd2SfoE7ojPNbeXl8MAaqU'
+
+class Mapview {
+    constructor(props = {
+        lat: 13.779820829768585,
+        lng: 100.54464812602707
+    })
+}
